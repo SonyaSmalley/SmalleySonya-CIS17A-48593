c0dfabe7a6e249b9f76b6e5a12d96e0fc4323b02
SonyaSmalley <ssmalley1@student.rccd.edu>
2021-08-27 13:27:15 -0700

    Signed-off-by: SonyaSmalley <ssmalley1@student.rccd.edu>

---
+++ Lab/IntroLab/HelloWorld/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File: main.cpp
+ * Author: Sonya Smalley
+ * Created on August 26, 2021, 12:22 PM
+ * Purpose: Say hello
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+    //Set Random Number seed
+    
+    
+    //Declare Variable Data Types and Constants
+    
+    
+    //Initialize Variables
+
+
+    //Display Outputs
+    cout<<"Hello World";
+
+    //Exit stage right!
+    return 0;
+}
+
